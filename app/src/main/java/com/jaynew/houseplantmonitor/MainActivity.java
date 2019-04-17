e2e48363b117678db40f5f387f6f707e0f431350
Justin <jliu076@ucr.edu>
2019-04-16 20:01:24 -0700

    added two activity children

--- app/src/main/java/com/jaynew/houseplantmonitor/MainActivity.java
+++ app/src/main/java/com/jaynew/houseplantmonitor/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jaynew.houseplantmonitor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button plant_one_activity = (Button)findViewById(R.id.plantButtonOne);
+        Button plant_two_activity = (Button)findViewById(R.id.plantButtonTwo);
+
+        plant_one_activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), PlantOneActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+        plant_two_activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), PlantTwoActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 
 
