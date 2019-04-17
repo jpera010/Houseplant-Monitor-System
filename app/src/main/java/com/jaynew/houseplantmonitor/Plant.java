2eb65d65f7ed4544f1fa12fe6ad41963c67bfdab
Justin <jliu076@ucr.edu>
2019-04-16 19:31:38 -0700

    added variables to Plant class

--- app/src/main/java/com/jaynew/houseplantmonitor/Plant.java
+++ app/src/main/java/com/jaynew/houseplantmonitor/Plant.java
@@ -1,27 +1,40 @@
 package com.jaynew.houseplantmonitor;
 
-public class Plant {
+public class Plant {                    //Plant class to keep track of data of plants, updated by FirebaseHelper
+    private String name;
     private int temperature;
     private int moisture_level;
 
-    public Plant(int temperature, int moisture_level) {             //Plant class to keep track of data of plants, updated by FirebaseHelper
+    public Plant() {
+    }
+
+    public Plant(String name, int temperature, int moisture_level) {
+        this.name = name;
         this.temperature = temperature;
         this.moisture_level = moisture_level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getTemperature() {
         return temperature;
     }
 
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
     public int getMoisture() {
         return moisture_level;
     }
 
-    public void updateTemperature(int new_temperature) {
-        this.temperature = new_temperature;
-    }
-
-    public void updateMoisture(int new_moisture_level) {
-        this.moisture_level = new_moisture_level;
+    public void setMoisture(int moisture_level) {
+        this.moisture_level = moisture_level;
     }
 }
