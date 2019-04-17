bc8af6cb01e29ed0854590c2d7d20a6922bb6794
Justin <jliu076@ucr.edu>
2019-04-17 16:55:55 -0700

    added 2nd activity + dynamic/automatic updates to database

--- app/src/main/java/com/jaynew/houseplantmonitor/PlantOneActivity.java
+++ app/src/main/java/com/jaynew/houseplantmonitor/PlantOneActivity.java
@@ -3,9 +3,6 @@ package com.jaynew.houseplantmonitor;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,17 +11,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
-import java.util.List;
-
 public class PlantOneActivity extends AppCompatActivity {
 
-    private TextView plantTextOne;
-    private TextView temperatureView;
-    private TextView moistureView;
-    private Button showButton;
-    //private String key;
+    private TextView plantTextA;
+    private TextView temperatureA;
+    private TextView moistureViewA;
     DatabaseReference reff;
 
     @Override
@@ -32,37 +23,27 @@ public class PlantOneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant_one);
 
-        plantTextOne = (TextView)findViewById(R.id.plantTextOne);
-        temperatureView = (TextView)findViewById(R.id.temperatureView);
-        moistureView = (TextView)findViewById(R.id.moistureView);
-        showButton = (Button)findViewById(R.id.showButton);
+        plantTextA = (TextView)findViewById(R.id.plantTextA);
+        temperatureA = (TextView)findViewById(R.id.temperatureViewA);
+        moistureViewA = (TextView)findViewById(R.id.moistureViewA);
 
-        showButton.setOnClickListener(new View.OnClickListener() {
+        reff = FirebaseDatabase.getInstance().getReference().child("plantData").child("1");
+        reff.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                reff = FirebaseDatabase.getInstance().getReference().child("plantData").child("1");
-                reff.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String name = dataSnapshot.child("name").getValue().toString();
-                        String temperature = dataSnapshot.child("temperature").getValue().toString();
-                        String moisture_level = dataSnapshot.child("moisture_level").getValue().toString();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue().toString();
+                String temperature = dataSnapshot.child("temperature").getValue().toString();
+                String moisture_level = dataSnapshot.child("moisture_level").getValue().toString();
 
-                        plantTextOne.setText(name);
-                        temperatureView.setText(temperature);
-                        moistureView.setText(moisture_level);
-                    }
+                plantTextA.setText(name);
+                temperatureA.setText(temperature);
+                moistureViewA.setText(moisture_level);
+            }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                    }
-                });
             }
         });
     }
-
-
-
-
 }
