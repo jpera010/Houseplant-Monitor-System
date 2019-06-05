d5e32c38bef9a805b0830faa3c53247e96b422ea
Akhil Silla <asill003@ucr.edu>
2019-06-05 03:06:42 -0700

    Update ArducamThree.ino

--- Arducam/ArducamThree/ArducamThree.ino
+++ Arducam/ArducamThree/ArducamThree.ino
@@ -4,7 +4,7 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
-#define Password_Length 5 
+#define Password_Length 5
 
 #include <Wire.h> // Used to establied serial communication on the I2C bus
 #include "SparkFunTMP102.h" // Used to send and recieve specific information from our sensor
@@ -16,7 +16,7 @@ TMP102 sensor0(0x48);
 
 enum temp_states {temp_off, temp_read, temp_output} temp_state;
 float Temp[array_length]; // array of values to average
-int Tarray_index; 
+int Tarray_index;
 float temp_total; // running total
 float temperature; // actual value to return
 float temp_reading;
@@ -24,6 +24,7 @@ bool signalDetected;
 bool readingData;
 float sensor_reading;
 bool unsafeTemp;
+bool lockEventValue;
 float minTemp = 74;
 float maxTemp = 76;
 
@@ -31,7 +32,7 @@ int signalPin = 15;  // activates output pin 15 for LED
 
 char Data[Password_Length]; // array to store user keypad input
 String MasterPasscode = "";
-//char Master[Password_Length] = "1234"; 
+//char Master[Password_Length] = "1234";
 byte data_count = 0, master_count = 0;
 bool unlocked;
 char customKey;
@@ -55,13 +56,13 @@ Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS)
 
 #define FIREBASE_HOST "houseplant-monitor.firebaseio.com"
 #define FIREBASE_AUTH "1N7ntYmFD3HCJngEVKeKtAbVCJEBEK7TOVpoNLnb"
-#define WIFI_SSID "Justin's P10"
-#define WIFI_PASSWORD "thatsucc"
+#define WIFI_SSID "FirePuff12"
+#define WIFI_PASSWORD "VolcaMang0rona715"
 
 
-void setup(){
+void setup() {
   Serial.begin(115200);
-// connect to wifi.
+  // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -72,26 +73,58 @@ void setup(){
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.stream("");
   initializeVariables();
   initializeTempSensor();
   pinMode(signalPin, OUTPUT);
-  unlocked = !(Firebase.getInt("tools/lock"));
-  if (!unlocked) {
-    digitalWrite(signalPin, LOW);
-  }
-  else {
-    digitalWrite(signalPin, HIGH);
-  }
+  //unlocked = !(Firebase.getInt("security/lockSet"));
+  //if (!unlocked) {
+  //  digitalWrite(signalPin, LOW);
+ // }
+ // else {
+ //   digitalWrite(signalPin, HIGH);
+ // }
 }
 
-void loop(){
-  MasterPasscode = Firebase.getString("keypadPasscode");
-  signalDetected = Firebase.getInt("alerts/updateSensors"); 
+void loop() {
+  if (Firebase.available())
+  {
+    FirebaseObject event = Firebase.readEvent();
+    String eventPath = event.getString("path");
+    Serial.print("Event path is: ");
+    Serial.println(eventPath);
+    if (eventPath == "/security/lockSet")
+    {
+      Serial.println("lockSet");
+      lockEventValue = event.getBool("data");
+      if (lockEventValue == false) {
+        digitalWrite(signalPin, LOW);
+        //Serial.println("System is open");
+      }
+      else {
+        digitalWrite(signalPin, HIGH);
+        //Serial.println("System is closed");
+      }
+    }
+    if (eventPath == "/security/keypadPasscode")
+    {
+      MasterPasscode = event.getString("data");
+      //Serial.println(MasterPasscode);
+    }
+    if (eventPath == "/signals/updateSensors")
+    {
+      signalDetected = event.getBool("data");
+      //Serial.println(signalDetected);
+    }
+  }
+
+  //MasterPasscode = Firebase.getString("keypadPasscode");
+  //signalDetected = Firebase.getInt("signal/updateSensors");
   //char const *Master = MasterPasscode.c_str();
   customKey = customKeypad.getKey();
   runKeypad(customKey);
   runSensor(signalDetected);
-}  
+}
 
 void runSensor(bool b) {
   if (b) {
@@ -101,82 +134,82 @@ void runSensor(bool b) {
     while (readingData) {
       run_tempSM();
       delay(1000); // read every 1 second
-    }   
+    }
   }
   return;
 }
 
 void runKeypad(char a) {
-  if (a){ 
+  if (a) {
     digitalWrite(signalPin, HIGH);
     Serial.println(a);
-    Data[data_count] = a; 
+    Data[data_count] = a;
     data_count++;
     delay(50);
-    digitalWrite(signalPin, LOW); 
+    digitalWrite(signalPin, LOW);
   }
   if (unlocked) {
     if (customKey == '*') {
       unlocked = 0;
-      Firebase.setInt("tools/lock", 1);
+      Firebase.setInt("security/lockSet", 1);
       digitalWrite(signalPin, LOW);
-      while(data_count !=0){
-        Data[data_count--] = 0; 
+      while (data_count != 0) {
+        Data[data_count--] = 0;
       }
     }
   }
   else {
     //if input count = 4
-    if(data_count == Password_Length-1){ 
-      if(!strcmp(Data, MasterPasscode.c_str())){  // strcmp returns 0 if equal
+    if (data_count == Password_Length - 1) {
+      if (!strcmp(Data, MasterPasscode.c_str())) { // strcmp returns 0 if equal
         unlocked = 1;
-        Firebase.setInt("tools/lock", 0);
+        Firebase.setInt("security/lockSet", 0);
         digitalWrite(signalPin, HIGH); //on for 5 seconds
       }
-      else{ //incorrect password, blink on/off
-        digitalWrite(signalPin, HIGH); 
+      else { //incorrect password, blink on/off
+        digitalWrite(signalPin, HIGH);
         delay(250);
         digitalWrite(signalPin, LOW);
         delay(250);
-        digitalWrite(signalPin, HIGH); 
+        digitalWrite(signalPin, HIGH);
         delay(250);
         digitalWrite(signalPin, LOW);
         delay(250);
-        digitalWrite(signalPin, HIGH); 
+        digitalWrite(signalPin, HIGH);
         delay(250);
         digitalWrite(signalPin, LOW);
       }
-      while(data_count !=0){
-        Data[data_count--] = 0; 
+      while (data_count != 0) {
+        Data[data_count--] = 0;
       }
-    }  
+    }
   }
 }
 
 void updateTemp(int temp, int tempAlert) {
-  Firebase.setInt("alerts/unsafeTemp", tempAlert); 
+  Firebase.setInt("alerts/unsafeTemp", tempAlert);
   if (Firebase.failed()) {
-      Serial.print("pushing unsafe temp alert failed:");
-      Serial.println(Firebase.error());  
-      return;
+    Serial.print("pushing unsafe temp alert failed:");
+    Serial.println(Firebase.error());
+    return;
   }
-  Firebase.setInt("alerts/updateSensors", 0); 
+  Firebase.setInt("signals/updateSensors", 0);
   if (Firebase.failed()) {
-      Serial.print("pushing update sensor signal failed:");
-      Serial.println(Firebase.error());  
-      return;
+    Serial.print("pushing update sensor signal failed:");
+    Serial.println(Firebase.error());
+    return;
   }
   delay(1000);
-  Firebase.setInt("plantData/temperature", temp); 
+  Firebase.setInt("plantData/temperature", temp);
   if (Firebase.failed()) {
-      Serial.print("pushing temperature failed:");
-      Serial.println(Firebase.error());  
-      return;  
+    Serial.print("pushing temperature failed:");
+    Serial.println(Firebase.error());
+    return;
   }
 }
 
 void initializeTempSensor() {
-  pinMode(ALERT_PIN,INPUT);  // Declare alertPin as an input
+  pinMode(ALERT_PIN, INPUT); // Declare alertPin as an input
   sensor0.begin();  // Join I2C bus
   sensor0.setFault(0);  // Trigger alarm immediately
   sensor0.setAlertPolarity(1); // Active HIGH
@@ -187,7 +220,7 @@ void initializeTempSensor() {
 
 void initializeVariables() {
   temp_state = temp_off;
-  signalDetected = false;
+  //signalDetected = false;
   readingData = false;
   sensor_reading = 0;
   temperature = 0;
@@ -196,52 +229,52 @@ void initializeVariables() {
 }
 
 void run_tempSM() {
-    switch(temp_state) {
-      case temp_off:
-        Serial.println("Starting sensor readings");
-        Tarray_index = 0;
-        temp_total = 0;
-        temperature = 0;
-        unsafeTemp = 0;
-        for (int i = 0; i < array_length; i++) { // initialize array to zeros
-          Temp[i] = 0;
-        }
+  switch (temp_state) {
+    case temp_off:
+      Serial.println("Starting sensor readings");
+      Tarray_index = 0;
+      temp_total = 0;
+      temperature = 0;
+      unsafeTemp = 0;
+      for (int i = 0; i < array_length; i++) { // initialize array to zeros
+        Temp[i] = 0;
+      }
+      temp_state = temp_read;
+      break;
+
+    case temp_read:
+      //Serial.print("Reading Temperature ");
+      Serial.println(Tarray_index + 1);
+      sensor_reading = readTemp();
+      Temp[Tarray_index] = sensor_reading;  // add reading to array
+      Serial.print("Sensor reading is ");
+      Serial.println(sensor_reading);
+      Serial.print("Sensor out of range? ");
+      unsafeTemp = (sensor_reading < minTemp || sensor_reading > maxTemp);
+      Serial.println(unsafeTemp);
+      temp_total += Temp[Tarray_index]; // add reading to total
+      Tarray_index += 1; // increment array index
+      if (Tarray_index >= array_length) {
+        temp_state = temp_output;
+      }
+      else {
         temp_state = temp_read;
-        break;  
-      
-      case temp_read:
-        //Serial.print("Reading Temperature ");
-        Serial.println(Tarray_index + 1);
-        sensor_reading = readTemp();
-        Temp[Tarray_index] = sensor_reading;  // add reading to array
-        Serial.print("Sensor reading is ");
-        Serial.println(sensor_reading);
-        Serial.print("Sensor out of range? ");
-        unsafeTemp = (sensor_reading < minTemp || sensor_reading > maxTemp);
-        Serial.println(unsafeTemp);
-        temp_total += Temp[Tarray_index]; // add reading to total
-        Tarray_index += 1; // increment array index
-        if (Tarray_index >= array_length) {
-          temp_state = temp_output;
-        }
-        else {
-          temp_state = temp_read;
-        }
-        break;
-        
-      case temp_output:
-        Serial.println("Done reading Temperature");
-        readingData = false;
-        temperature = temp_total / array_length;
-        int temp1 = int(temperature);
-        temp_state = temp_off;
-        Serial.print("Temperature is ");
-        Serial.println(temperature);
-        Serial.println("");
-        unsafeTemp = (temperature < minTemp || temperature > maxTemp);
-        updateTemp(temperature, unsafeTemp);
-        break;
-    }
+      }
+      break;
+
+    case temp_output:
+      Serial.println("Done reading Temperature");
+      readingData = false;
+      temperature = temp_total / array_length;
+      int temp1 = int(temperature);
+      temp_state = temp_off;
+      Serial.print("Temperature is ");
+      Serial.println(temperature);
+      Serial.println("");
+      unsafeTemp = (temperature < minTemp || temperature > maxTemp);
+      updateTemp(temperature, unsafeTemp);
+      break;
+  }
 }
 
 float readTemp() {
